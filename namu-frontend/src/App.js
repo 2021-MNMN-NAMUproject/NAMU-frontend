03208d6c4db7cf651470c25b08eb7c595d54cc19
hyunjoebrother <hyunjoe31@gmail.com>
2021-07-23 22:05:25 +0900

    feat: implement App.js

--- namu-frontend/src/App.js
+++ namu-frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Headerbar from "./components/Headerbar";
 import Visual from "./components/Visual";
+import
 import styled from "styled-components";
 
 function App() {
